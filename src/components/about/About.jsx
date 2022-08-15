130b43ba2804e9a1b69203fb782fb13f5d4754f6
Stephanie <stephaniiehallberg@gmail.com>
2022-08-15 22:47:39 +0200

    feat(portfolio): update portfolio

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import './about.css'
-import ME from '../../assets/me-about.jpg'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
@@ -20,28 +18,35 @@ const About = () => {
 
         <div className="about__content">
           <div className="about__cards">
-            <article className='about__card'>
-              <FaAward className='about__icon'/>
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>4+ Years Working</small>
             </article>
 
-            <article className='about__card'>
-              <VscFolderLibrary className='about__icon'/>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
-              <small>15+ Completed</small>
+              <small>Automotive, Media Marketing, Health Care & Security</small>
             </article>
           </div>
 
-          <p>
-          Hi! I'm Stephanie. I've experience from the automotive industry in new car projects, interface development, quality requirements, automated tests and vehicle components. Trained System Developer in Java / C# ASP.NET with specialization in T - SQL, Test and Agile methods. Have a bachelor’s in system science. "Stephanie is goal oriented and with professional attitude fulfills her duties with great skill".
-          </p>
+          <h2 style={{marginTop: '2.5rem'}}>
+            I'm Stephanie, a frontend & backend developer in Gothenburg.
+          </h2>
+          <h3>
+            HI there! My main expertise lies in frontend development, thought I
+            do dabble in UX with Figma, backend coding and database management.
+            If you like my site, don’t hesitate to reach out! More to come soon.
+          </h3>
 
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          <a style={{marginTop: '1.5rem'}} href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,86 +1,115 @@
-import React from 'react'
-import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
-
-// DO NOT USE THE IMAGES IN PRODUCTION
+import "./portfolio.css";
 
 const data = [
   {
-    id: 1,
-    image: 'https://github.com/stephyswe/crypto/blob/master/logo.png?raw=true',
-    title: 'Crypto Currency Dashboard & Financial Visualization',
-    github: 'https://github.com/stephyswe/crypto',
-    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+    image:
+      "https://raw.githubusercontent.com/stephyswe/next-nuber-client/main/preview.png",
+    title: "Uber Eats Clone",
+    github: "https://github.com/stephyswe/next-nuber-client",
+    demo: "https://next-nuber-client.vercel.app/client/delivery",
   },
   {
-    id: 2,
-    image: 'https://github.com/stephyswe/carzone/raw/master/preview.png',
-    title: 'Automotive car selling and re-buying',
-    github: 'https://github.com/stephyswe/carzone',
-    demo: 'https://glacial-retreat-43749.herokuapp.com/'
+    image:
+      "https://raw.githubusercontent.com/stephyswe/next-movie-app/main/preview.png",
+    title: "Movie Time!",
+    github: "https://github.com/stephyswe/next-movie-app",
+    demo: "https://next-movie-app-mu.vercel.app",
   },
   {
-    id: 3,
-    image: 'https://github.com/stephyswe/devchat/raw/master/header.png',
-    title: 'Slack clone with interactive chat and messages',
-    github: 'https://github.com/stephyswe/devchat',
-    demo: 'https://github.com/stephyswe/devchat/raw/master/header.png'
+    image:
+      "https://github.com/stephyswe/react-tinyhouse-replica/raw/main/preview.png?raw=true",
+    title: "Tiny House ( AirBnB Clone )",
+    github: "https://github.com/stephyswe/airbnb-clone",
+    demo: "https://tinyhouse-app-clone.herokuapp.com/",
   },
   {
-    id: 4,
-    image: 'https://github.com/stephyswe/devconnect/raw/master/preview.png',
-    title: 'DevConnect - Social Network for Developers',
-    github: 'https://github.com/stephyswe/devconnect',
-    demo: 'https://dribbble.com/shots/16955822-Maintaining-tasks-and-tracking-progress'
+    image: "https://github.com/stephyswe/carzone/raw/master/preview.png",
+    title: "Automotive car selling and re-buying",
+    github: "https://github.com/stephyswe/carzone",
+    demo: "https://glacial-retreat-43749.herokuapp.com/",
   },
   {
-    id: 5,
-    image: 'https://github.com/stephyswe/next-linkedin-clone/raw/main/preview.png?raw=true',
-    title: 'LinkedIn Clone in NextJS',
-    github: 'https://github.com/stephyswe/next-linkedin-clone',
-    demo: 'https://next-linkedin-clone-chi.vercel.app/'
+    image: "https://github.com/stephyswe/devchat/raw/master/header.png",
+    title: "Slack clone with interactive chat and messages",
+    github: "https://github.com/stephyswe/devchat",
+    demo: "https://react-slack-clone-d5116.firebaseapp.com",
+    subtitle:
+      "login in with user@gmail.com / password12 or create new account.",
   },
   {
-    id: 6,
-    image: 'https://github.com/stephyswe/next-spotify-clone/raw/main/preview.png?raw=true',
-    title: 'Spotify Clone in NextJS',
-    github: 'https://github.com/stephyswe/next-spotify-clone',
-    demo: 'http://next-spotify-clone-mocha.vercel.app/'
-  }
-]
-
+    image: "https://github.com/stephyswe/geotourism/raw/master/map.png",
+    title: "Geo Tourism - Places to Visit in Gothenburg",
+    github: "https://github.com/stephyswe/geotourism",
+    demo: "https://geotourism-73jp8ldgi.now.sh/",
+  },
+  {
+    image: "https://github.com/stephyswe/devconnect/raw/master/preview.png",
+    title: "DevConnect - Social Network for Developers",
+    github: "https://github.com/stephyswe/devconnect",
+    demo: "https://react-devconnector-community.herokuapp.com/",
+  },
+  {
+    image:
+      "https://github.com/stephyswe/next-linkedin-clone/raw/main/preview.png?raw=true",
+    title: "LinkedIn Clone in NextJS",
+    github: "https://github.com/stephyswe/next-linkedin-clone",
+    demo: "https://next-linkedin-clone-chi.vercel.app/",
+  },
+  {
+    image:
+      "https://github.com/stephyswe/next-spotify-clone/raw/main/preview.png?raw=true",
+    title: "Spotify Clone in NextJS",
+    github: "https://github.com/stephyswe/next-spotify-clone",
+    demo: "http://next-spotify-clone-mocha.vercel.app/",
+    subtitle: "require premium Spotify account to play music.",
+  },
+  {
+    image: "https://github.com/stephyswe/crypto/blob/master/logo.png?raw=true",
+    title: "Crypto Currency Dashboard & Financial Visualization",
+    github: "https://github.com/stephyswe/crypto",
+    demo: "https://cryptodash-2331zd4pn.now.sh/",
+  },
+];
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {
-          data.map(({id, image, title, github, demo}) => {
-            return (
-              <article key={id} className='portfolio__item'>
+        {data.map(({ id, image, title, github, demo, subtitle }) => {
+          return (
+            <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
               <div className="portfolio__item-cta">
-                <a href={github} className='btn' target='_blank'>Github</a>
-                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
               </div>
+              <p>{subtitle}</p>
             </article>
-            )
-          })
-        }
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
